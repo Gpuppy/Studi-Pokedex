8aab5099bc6a43a210f92bbaef6d2eccdcda0dac
Gael Adam <gaelswim@hotmail.com>
2024-01-05 15:50:16 +0100

    heroku database create

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-
-
 <body >
 <?php
+
+//Get Heroku ClearDB connection information
+$jawsdb_url = parse_url(getenv("JAWSDB_DATABASE_URL"));
+$jawsdb_server = $jawsdb_url["host"];
+$jawsdb_username = $jawsdb_url["user"];
+$jawsdb_password = $jawsdb_url["pass"];
+$jawsdb_db = substr($jawsdb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
+
 
 require ("header.php");
 require ("./PokemonsManager.php");
